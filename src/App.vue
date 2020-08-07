22e0cf7916650d285f913819f3df3a7bda027d28
ZhangS97 <376195987@qq.com>
2020-08-07 21:32:17 +0800

    代码格式化
    代码格式化

--- src/App.vue
+++ src/App.vue
@@ -3,72 +3,48 @@
     <el-container>
       <el-header>
         <el-row :gutter="20">
-  <el-col :span="18">
-    <el-input v-model="inputField"
-                    @keyup.enter.native="addTodo"                  
- placeholder="Todo Item"></el-input></el-col>
-  
-  <el-col :span="6"> <el-button @click="addTodo" type="primary">Add Todo</el-button></el-col>
-  
-</el-row>
-        
+          <el-col :span="18">
+            <el-input v-model="inputField" @keyup.enter.native="addTodo" placeholder="今天要做哪些事呢？"></el-input>
+          </el-col>
 
-  
+          <el-col :span="6">
+            <el-button @click="addTodo" type="primary">添加</el-button>
+          </el-col>
+        </el-row>
       </el-header>
       <el-main>
-        <el-row :gutter="20"
-         v-for="todo in todoList"
-                :key="todo.id">
-                <el-col :span="6">
-                               <input
-                    type="checkbox"
-                    v-on:change="toggle(todo)"
-                    v-bind:checked="todo.complete"
-                    class="col-sm-1 border border-danger"
-                  >
-                </el-col>
+          <el-row :gutter="20" v-for="todo in todoList" :key="todo.id">
+            <el-col :span="12">
+              <del v-if="todo.complete">
+                <h5>{{ todo.name }}</h5>
+              </del>
+              <span v-else>
+                <h5>{{ todo.name }}</h5>
+              </span>
+            </el-col>
 
-<el-col :span="12">
-                  <del
-                    v-if="todo.complete"
-                    class="col-sm-8"
-                  >
-                    <h5>{{ todo.name }}</h5>
-                  </del>
-                  <span
-                    v-else
-                    class="col-sm-8"
-                  >
-                    <h5>{{ todo.name }}</h5>
-                  </span>
-                </el-col>
+            <el-col :span="6">
+              <el-button type="primary" @click="deleteTodo(todo)" round>完成</el-button>
+            </el-col>
 
-
-
-                 <el-col :span="6">
-                     <el-button type="success" icon="el-icon-check" @click="deleteTodo(todo)" circle></el-button>
-                 </el-col>
-
-        </el-row>
-        
+            <el-col :span="6">
+              <el-button type="danger" icon="el-icon-delete" @click="deleteTodo(todo)" circle></el-button>
+            </el-col>
+          </el-row>
       </el-main>
-
     </el-container>
-
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-  components: {
-  },
+  name: "App",
+  components: {},
   methods: {
     addTodo: function (todo) {
       todo = this.inputField;
       this.todoList.push({ name: todo, complete: false });
-      this.inputField = '';
+      this.inputField = "";
       console.log(this.todoList);
     },
     deleteTodo: function (todo) {
@@ -78,18 +54,17 @@ export default {
     },
     toggle: function (todo) {
       todo.complete = !todo.complete;
-    }
+    },
   },
   data() {
     return {
-      inputField: '',
-      todoList: []
-    }
-  }
-
-
-}
+      inputField: "",
+      todoList: [],
+    };
+  },
+};
 </script>
 
 <style>
+
 </style>
