4097c8ba20a9bd3de401d8f212a4368613f56e25
ZhangS97 <376195987@qq.com>
2020-08-07 23:14:41 +0800

    存档保命

--- src/App copy.vue
+++ src/App copy.vue
@@ -1,18 +1,6 @@
 <template>
   <div id="app">
     <el-container>
-
-<el-tabs v-model="editableTabsValue" type="card" editable @edit="handleTabsEdit">
-  <el-tab-pane
-    :key="item.name"
-    v-for="item in editableTabs"
-    :label="item.title"
-    :name="item.name"
-  >
-    {{item.content}}
-  </el-tab-pane>
-</el-tabs>
-
       <el-header>
         <el-row :gutter="20">
           <el-col :span="18">
@@ -53,34 +41,6 @@ export default {
   name: "App",
   components: {},
   methods: {
-    handleTabsEdit(targetName, action) {
-        if (action === 'add') {
-          let newTabName = ++this.tabIndex + '';
-          this.editableTabs.push({
-            title: 'New Tab',
-            name: newTabName,
-            content: 'New Tab content'
-          });
-          this.editableTabsValue = newTabName;
-        }
-        if (action === 'remove') {
-          let tabs = this.editableTabs;
-          let activeName = this.editableTabsValue;
-          if (activeName === targetName) {
-            tabs.forEach((tab, index) => {
-              if (tab.name === targetName) {
-                let nextTab = tabs[index + 1] || tabs[index - 1];
-                if (nextTab) {
-                  activeName = nextTab.name;
-                }
-              }
-            });
-          }
-          
-          this.editableTabsValue = activeName;
-          this.editableTabs = tabs.filter(tab => tab.name !== targetName);
-        }
-      },
     addTodo: function (todo) {
       todo = this.inputField;
       this.todoList.push({ name: todo, complete: false });
@@ -100,21 +60,9 @@ export default {
     return {
       inputField: "",
       todoList: [],
-      editableTabsValue: '2',
-        editableTabs: [{
-          title: 'Tab 1',
-          name: '1',
-          content: 'Tab 1 content'
-        }, {
-          title: 'Tab 2',
-          name: '2',
-          content: 'Tab 2 content'
-        }],
-        tabIndex: 2
-      }
-    }
-  
-}
+    };
+  },
+};
 </script>
 
 <style>
